b52075ea3f3b375dfcefe9513f4a68ef8dcf24a5
LEE HYEONHO <play_love@likelion.org>
2018-01-10 04:41:07 +0000

    record 생성 user 연결

--- app/controllers/closet_controller.rb
+++ app/controllers/closet_controller.rb
@@ -1,9 +1,26 @@
 class ClosetController < ApplicationController
+  before_action :authenticate_user!, :except => [:index]
+
   def index
-  
+    #로그인 되어있으면 calendar page로
+    if current_user != nil
+      redirect_to "/calendar"
+    end
+    #navbar  비활성화
+    @disable_nav = true
   end
-  
+
   def calendar
-    
+    #@cal_record = Record.find_by(user: current_user.id)
+  end
+
+  def enrollment
+    @record = Record.new
+    @record.memo = params[:memo]
+    @record.written_date = params[:written_date]
+    @record.user_id = current_user.id
+    @record.save
+
+    redirect_to "/calendar"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109111555) do
+ActiveRecord::Schema.define(version: 20180110043214) do
+
+  create_table "records", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.string "memo"
+    t.string "image_url"
+    t.string "written_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
